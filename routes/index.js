80001873f409c3d0df99fb5e38f61fb82c49e3ac
Danino <maxime.danino04@gmail.com>
2025-02-05 16:58:44 +0100

    Ajout route tickets

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,45 @@
 var express = require('express');
 var router = express.Router();
 const Ticket = require("../models/ticket.js");
+const moment = require("moment");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get("/tickets", async (req, res) => {
+  try {
+      const departure = req.body.departure;
+      const arrival = req.body.arrival;
+      const date = req.body.date;
+
+      if (!departure || !arrival || !date) 
+      {
+          return res.json({ result: false, error: "Les champs 'departure', 'arrival' et 'date' sont obligatoires." });
+      }
+
+      const momentDate = moment.utc(date, "YYYY-MM-DD HH:mm", true);
+      if (!momentDate.isValid()) 
+      {
+          return res.json({ result: false, error: "La date fournie est invalide. Veuillez utiliser le format 'YYYY-MM-DD HH:mm'." });
+      }
+      const endOfDay = momentDate.clone().endOf("day"); 
+
+      const tickets = await Ticket.find({departure: departure,arrival: arrival,date: { $gte: momentDate.toDate(), $lte: endOfDay.toDate() }});
+
+
+      if (tickets.length === 0) {
+          return res.json({ result: false, error: "Aucun ticket trouvé avec les critères fournis." });
+      }
+
+      res.json({ result: true, tickets: tickets });
+
+  } catch (err) {
+      res.json({ result: false, error: "Erreur serveur, veuillez réessayer plus tard." });
+  }
+});
+
+
 
 module.exports = router;
