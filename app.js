df794b571cf6f7f20df1bc1c180474749e37a036
Danino <maxime.danino04@gmail.com>
2025-02-06 09:12:45 +0100

    Ajout de nouvelles route

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ require("dotenv").config();
 require("./models/connection");
 
 var app = express();
-
+const cors = require('cors');
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
